e60ef5e73811607f63fc07d08c8bf5dd7186d4df
Wahyu Safrizal <wahyusafrizal174@gmail.com>
2025-08-26 22:17:25 +0700

    update

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
-use App\Models\Product;
 use App\Models\Brand;
 use App\Models\Multiimg;
 use App\Models\Products;
